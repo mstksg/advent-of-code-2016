b65566714811177224f120ea27c929aad3214d44
Justin Le <justin@jle.im>
2020-11-23 09:15:55 -0800

    day 7

--- src/AOC/Challenge/Day07.hs
+++ src/AOC/Challenge/Day07.hs
@@ -22,22 +22,53 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day07 (
-    -- day07a
-  -- , day07b
+    day07a
+  , day07b
   ) where
 
 import           AOC.Prelude
+import qualified Data.Set as S
 
-day07a :: _ :~> _
+day07a :: [([String], [String])] :~> Int
 day07a = MkSol
-    { sParse = Just
+    { sParse = Just . map chunkUp . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . length . filter (uncurry validIP)
     }
+  where
+    validIP outer inner = not (null (concatMap findAbba outer))
+                       && null (concatMap findAbba inner)
 
-day07b :: _ :~> _
+day07b :: [([String], [String])] :~> Int
 day07b = MkSol
-    { sParse = Just
+    { sParse = Just . map chunkUp . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . length . filter (uncurry validIP)
     }
+  where
+    validIP outer inner = not (S.null (S.intersection abaO abaI))
+      where
+        abaO = S.fromList (concatMap findAba outer)
+        abaI = S.fromList (swap <$> concatMap findAba inner)
+
+findAbba :: String -> [(Char, Char)]
+findAbba (a:b:c:d:xs)
+  | a /= b && a == d && b == c = (a,b) : findAbba (b:c:d:xs)
+  | otherwise                  = findAbba (b:c:d:xs)
+findAbba _ = []
+
+findAba :: String -> [(Char, Char)]
+findAba (a:b:c:xs)
+  | a == c && a /= b = (a,b) : findAba (b:c:xs)
+  | otherwise        = findAba (b:c:xs)
+findAba _ = []
+
+chunkUp :: String -> ([String], [String])
+chunkUp = outOfBracket
+  where
+    outOfBracket str = case span (/= '[') str of
+      (xs, []  ) -> ([xs], [])
+      (xs, _:ys) -> first (xs:) (inBracket ys)
+    inBracket str = case span (/= ']') str of
+      (xs, []  ) -> ([], [xs])
+      (xs, _:ys) -> second (xs:) (outOfBracket ys)
